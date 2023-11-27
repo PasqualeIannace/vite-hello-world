d566cf30d5654a827baf0c913c0e0ac060047975
PasqualeIannace <pasquale.iannace@gmail.com>
2023-11-27 17:55:18 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+import HelloVite from './components/HelloVite.vue'
+
+export default {
+  components: {
+    HelloVite
+  }
+}
+</script>
+
+<template>
+  <h1>Hello Vite</h1>
+  <HelloVite />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/HelloVite.vue
@@ -0,0 +1,11 @@
+<script>
+
+</script>
+
+<template>
+  <p>La mia prima app con Vite!</p>
+</template>
+
+<style>
+
+</style>
